7245bc68339d9e49610bbdddaf839c9bb935bea6
dmswjd9501 <dmswjd9501@soongsil.ac.kr>
2019-07-31 17:16:00 +0900

    190731 | practice01

---
+++ 190731/List.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('input2.txt', 'r')
+
+# view 문제
+for tc in range(1, 11): # 1부터 10까지 tc 반복
+    N = int(input()) # 테스트케이스의 길이
+    arr = list(map(int, input().split()))
+
+    result = 0
+    for i in range(2, len(arr)-2):
+        if arr[i] > arr[i-2] and arr[i] > arr[i-1] and arr[i] > arr[i+1] and arr[i] > arr[i+2]:
+            result += arr[i] - max(arr[i-2], arr[i-1], arr[i+1], arr[i+2])
+
+    print('#{} {}'.format(tc, result))
