f46aa25eea86eaf89694a9212f6647f841a17870
dmswjd9501 <dmswjd9501@soongsil.ac.kr>
2019-08-09 09:00:16 +0900

    190808 | algorithms(최종본)

--- 190808/sw5_electric_stick.py
+++ 190808/sw5_electric_stick.py
@@ -2,28 +2,36 @@ import sys
 
 sys.stdin = open('input5.txt', 'r')
 
-tc = int(input()) # 테스트케이스 개수
-
-for t in range(1, tc+1):
-    n = int(input()) # 원형 금속 막대의 개수
-    arr = list(map(int, input().split())) # 굵기
-    a = []
-    b = []
-
-    for i in range(0, 2*n, 2): # 그룹으로 묶기
-        a.append([arr[i], arr[i+1]])
-
-    b.append(a.pop(0)) # a의 첫번째 값을 제거하여 b 리스트에 넣기
-
-    for index in range(len(a)):
-        if b[0][1] == a[index][0]:
-            b.append(a.pop(index))
-        elif b[0][0] == a[index][1]:
-            b.insert(0, a.pop(index))
-        else:
-            break
-
-    print(a)
-    print(b)
-
+tc = int(input())
+ 
+for t in range(1, tc + 1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    num = []
+    i = 0
+    a = ''
+    b = ''
+     
+    for _ in range(N):
+        num.append(arr[i:i+2])
+        i += 2
+    a += str(num[0].pop(0)) + ' ' + str(num[0].pop(0))
+    num.pop(0)
+     
+    while len(num) > 0:
+        j = 0
+        while len(num) > 0:
+            if int(a[-2:]) == num[j][0]:
+                a += ' ' + str(num[j].pop(0))+ ' ' +str(num[j].pop(0))
+                num.pop(j)
+                break
+            elif int(a[0:2]) == num[j][1]:
+                b += (str(num[j].pop(0)) + ' ' + str(num[j].pop(0)) + ' ' + a)
+                a = b
+                num.pop(j)
+                b = ''
+                break
+            j += 1
+ 
+    print('#{} {}'. format(t, a))
 
